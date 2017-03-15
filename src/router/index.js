7f86feba508d7306455ff015fc9f7266d13c3fbf
chang0022 <chang.0022@163.com>
2017-03-15 17:59:45 +0800

    add Mysql 支持

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import TodoList from '@/components/TodoList'
 
 Vue.use(Router)
 
@@ -8,8 +9,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/todolist',
+      name: 'Todo-List',
+      component: TodoList
+    },{
+      path: '*',
+      redirect: '/'
     }
   ]
 })
